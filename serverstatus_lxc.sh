7df6675c0c22bdd38f5fa807d526bdd6b8ac3af9
honeok8s <honeok7@gmail.com>
2024-07-10 17:11:20 +0800

    重构探针脚本逻辑

--- serverstatus_lxc.sh
+++ serverstatus_lxc.sh
@@ -2,67 +2,69 @@
 # Author: honeok
 # Blog: honeok.com
 # Desc: Script to manage serverstatus probe
-# Example: */1 * * * * $PWD/serverstatus_lxc.sh s250 >/dev/null 2>&1
-# Github: https://raw.githubusercontent.com/honeok8s/shell/main/serverstatus_lxc.sh
+# Example: */1 * * * * /root/serverstatus_kvm.sh s250 >/dev/null 2>&1
+# Github: https://raw.githubusercontent.com/honeok8s/shell/main/serverstatus_kvm.sh
 
 set -o errexit
-cd ~
+
+# ANSI颜色码
+yellow='\033[1;33m' # 用于提示信息
+red='\033[1;31m'    # 用于警告信息
+green='\033[1;32m'  # 用于成功信息
+white='\033[0m'     # 用于结束颜色设置
 
 PARAMS=("$@")
 
-# Check if the script is provided with required parameters
-if [[ $# -lt 1 ]];then echo "Usage:<server_id>" && exit
+if [[ $# -lt 1 ]];then
+  printf "${yellow}Usage: ./$0 [server_id]${white}\n" && exit 1
 elif [[ ! "${PARAMS}" =~ ^s[0-9]+$ ]];then
-  echo "Invalid server ID format. Please provide a valid server ID." && exit
+  printf "${red}Invalid server ID format.${white}\n" && exit 1
 fi
 
-# Function to synchronize timezone with Shanghai
-TIMEZONE(){
-  # Get the current timezone
-  ZONE=$(date -R | awk '{print $6}')
-  # If not in Shanghai timezone, synchronize it
+set_timezone(){
+  local ZONE=$(date -R | awk '{print $6}')
   if [[ "${ZONE}" != "+0800" ]];then
     timedatectl set-timezone Asia/Shanghai && systemctl restart cron 2>&1
   fi
 }
 
-# Function to manage server probe
-PROBE(){
-  # Count running instances of client-linux.py
+manage_probe(){
   local LOG_TIME=$(date '+%Y-%m-%d %H:%M:%S')
   local GLOBLE_TIME=$(date '+%H:%M:%S')
   local COUNTER=$(ps -ef | grep '[c]lient-linux.py' | wc -l)
 
-    # Download probe file and update configurations
   if [[ ! -f "./client-linux.py" ]]; then
-    # Retry downloading for up to 3 times
-    for ((i=1; i<=3; i++)); do
-      wget --no-check-certificate -qO client-linux.py 'https://raw.githubusercontent.com/cppla/ServerStatus/master/clients/client-linux.py' >/dev/null 2>&1 &&
-      sed -i 's#CU = "cu.tz.cloudcpp.com"#CU = "119.6.222.234"#g; s#CT = "ct.tz.cloudcpp.com"#CT = "www.sc189.net"#g; s#CM = "cm.tz.cloudcpp.com"#CM = "wap.sc.10086.cn"#g' client-linux.py
+    for ((i=1; i<=10; i++)); do
+      # 成都三网EndPoint
+      wget --no-check-certificate -qO client-linux.py 'https://raw.githubusercontent.com/honeok8s/conf/main/client-linux.py' -N >/dev/null 2>&1
+      if [[ $? -eq 0 && -s "./client-linux.py" ]]; then
+        break
+      fi
     done
+    if [[ ! -s "./client-linux.py" ]]; then
+      printf "${red}${LOG_TIME} [ERROR] Download file failed or file is empty.${white}\n"
+      exit 1
+    fi
   fi
 
-  # Restart probe if not running
   if [[ "${COUNTER}" == 0 ]]; then
     nohup python3 client-linux.py SERVER="127.0.0.1" USER="${PARAMS}" >/dev/null 2>&1 &
-    echo "${LOG_TIME} [WARN] Probe started." >> ./main.log
+    echo "${LOG_TIME} [INFO] Probe started." >> ./main.log
   fi
 
-  # Clear logs every noon
   if [[ "${GLOBLE_TIME}" > "12:00:00" ]] && [[ "${GLOBLE_TIME}" < "12:00:50" ]]; then
     rm -f ./*.log 2>&1
   fi
 
-  # Terminate probe process every night at 11:55 PM
   if [[ "${GLOBLE_TIME}" > "23:55:00" ]] && [[ "${GLOBLE_TIME}" < "23:55:50" ]] && [[ "${COUNTER}" -ge 1 ]]; then
     kill -15 $(pgrep -f '[c]lient-linux.py')
     echo "${LOG_TIME} [INFO] Probe process terminated." >> ./main.log
   fi
 }
 
-MAIN() {
-  TIMEZONE
-  PROBE
+main(){
+  set_timezone
+  manage_probe
 }
 
-MAIN
+main
