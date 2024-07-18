ec85dd9f57435761fd576f480ba14ddc80b77fae
honeok8s <honeok7@gmail.com>
2024-07-18 16:30:26 +0800

    修改部分逻辑

--- server_manager.sh
+++ server_manager.sh
@@ -17,28 +17,155 @@ white='\033[0m'     # 结束颜色设置
 
 download_dir="/opt/software"
 
-check_download_dir(){
-	[ ! -d $download_dir ] && mkdir $download_dir -p || true
+check_download_dir() {
+    if [ ! -d "$download_dir" ]; then
+        mkdir -p "$download_dir" || return 1
+    fi
+    return 0
 }
 
+check_mysql_installed() {
+	if command -v mysql &>/dev/null; then
+		mysql_version=$(mysql --version 2>&1)
+
+		if [[ "$mysql_version" =~ ([0-9]+\.[0-9]+\.[0-9]+) ]]; then
+			mysql_version="${BASH_REMATCH[1]}"
+			printf "${green}MySQL已安装,版本为:${mysql_version}${white}\n"
+			return 0
+		else
+			printf "${red}无法确定MySQL版本.${white}\n"
+			return 1
+		fi
+	else
+		printf "${red}MySQL未安装.${white}\n"
+		return 1
+	fi
+}
+
+# MySQL初始化
+initialize_mysql() {
+	local dbroot_passwd=$(openssl rand -base64 10)
+
+	# 检查是否已经初始化过 MySQL
+	if grep -q "MySQL init process done. Ready for start up." /var/log/mysqld.log; then
+		printf "${red}MySQL已经初始化过,请不要重复初始化.${white}\n"
+		return 1
+	fi
+
+	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${dbroot_passwd}';" > /tmp/mysql-init
+	if mysqld --initialize --user=mysql --init-file=/tmp/mysql-init; then
+		rm -f /tmp/mysql-init
+		printf "${green}通过指定配置文件初始化MySQL成功!${white}\n"
+		printf "${yellow}MySQL Root密码:$dbroot_passwd${white}\n"
+		return 0
+	else
+		printf "${red}MySQL初始化失败.${white}\n"
+		rm -f /tmp/mysql-init
+		return 1
+	fi
+}
+
+# 下载或生成my.cnf配置文件
+generate_mysql_config() {
+	if wget -q -O /etc/my.cnf https://raw.githubusercontent.com/honeok8s/conf/main/mysql/my-4C8G.cnf; then
+		printf "${green}MySQL配置文件已成功下载.${white}\n"
+	else
+	# 如果下载失败,使用EOF语法生成文件
+		cat <<EOF > /etc/my.cnf
+# For advice on how to change settings please see
+# http://dev.mysql.com/doc/refman/8.0/en/server-configuration-defaults.html
+[mysql]
+default-character-set=utf8mb4
+
+[mysqld]
+#
+# Remove leading # and set to the amount of RAM for the most important data
+# cache in MySQL. Start at 70% of total RAM for dedicated server, else 10%.
+# innodb_buffer_pool_size = 128M
+#
+# Remove the leading "# " to disable binary logging
+# Binary logging captures changes between backups and is enabled by
+# default. It's default setting is log_bin=binlog
+# disable_log_bin
+#
+# Remove leading # to set options mainly useful for reporting servers.
+# The server defaults are faster for transactions and fast SELECTs.
+# Adjust sizes as needed, experiment to find the optimal values.
+# join_buffer_size = 128M
+# sort_buffer_size = 2M
+# read_rnd_buffer_size = 2M
+#
+# Remove leading # to revert to previous value for default_authentication_plugin,
+# this will increase compatibility with older clients. For background, see:
+# https://dev.mysql.com/doc/refman/8.0/en/server-system-variables.html#sysvar_default_authentication_plugin
+# default-authentication-plugin=mysql_native_password
+port=3306
+default-storage-engine=INNODB # 默认存储引擎
+character-set-server=utf8mb4  # 字符集设置
+default-authentication-plugin=mysql_native_password # 默认身份验证插件
+skip-log-bin      # 禁用二进制日志功能
+skip-name-resolve # 禁用名称解析
+
+datadir=/var/lib/mysql
+socket=/var/lib/mysql/mysql.sock
+
+log-error=/var/log/mysqld.log
+pid-file=/var/run/mysqld/mysqld.pid
+
+slow_query_log = ON # 慢查询日志开启
+long_query_time = 5 # 慢查询阈值设置为5秒
+slow_query_log_file = /var/lib/mysql/slow.log
+
+max_connections=500 # 最大连接数
+tmp_table_size=32M  # 临时表大小
+myisam_sort_buffer_size=64M # MyISAM排序缓冲区大小
+innodb_log_buffer_size=256M # InnoDB日志缓冲区大小
+innodb_buffer_pool_size=1024M # InnoDB缓冲池大小
+innodb_log_file_size=1024M    # InnoDB日志文件大小
+innodb_open_files=500   # InnoDB打开文件数
+max_allowed_packet=256M # 最大允许的数据包大小
+max_connect_errors=100  # 最大连接错误数
+connect_timeout=60      # 连接超时时间
+net_read_timeout=60     # 网络读取超时时间
+log_timestamps = SYSTEM # 日志时间戳格式
+#expire_logs_days =2
+binlog_expire_logs_seconds=172800 # 二进制日志过期时间2天
+max_binlog_size=512M     # 最大二进制日志文件大小
+read_rnd_buffer_size =1M # 随机读缓冲区大小
+read_buffer_size =1M     # 读取缓冲区大小
+sort_buffer_size =1M     # 排序缓冲区大小
+sql_mode = STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION # SQL模式设置
+
+[client]
+default-character-set=utf8mb4
+socket=/var/lib/mysql/mysql.sock
+EOF
+	fi
+}
+
+# MySQL安装
 mysql_install() {
 	check_download_dir
-
-	printf "${yellow}在${os_release}上安装! ${white}\n"
+	if ! check_mysql_installed; then
+		printf "${yellow}在${os_release}上安装! ${white}\n"
+	else
+		printf "${red}MySQL已安装!${white}\n"
+		break
+	fi
 
 	cd "$download_dir" || exit 1
 
 	# 检查临时文件夹权限,由于mysql安装中会通过mysql用户在/tmp目录下新建tmp_db文件,所以给/tmp较大权限
 	if [ "$(stat -c '%A' /tmp)" != "drwxrwxrwt" ]; then
 		chmod -R 777 /tmp
-		printf "${yellow}已更新/tmp目录权限为777${white}\n"
+		printf "${yellow}已更新/tmp目录权限为777!${white}\n"
 	fi
 
 	# 检查并卸载冲突的包
 	for package in mariadb-libs mysql-libs; do
 		if rpm -q $package >/dev/null 2>&1; then
 			yum remove $package -y >/dev/null 2>&1
-			printf "${yellow} 正在卸载冲突文件${package}${white}\n"
+			printf "${yellow} 正在卸载冲突文件:${package}${white}\n"
 		fi
 	done
 
@@ -46,7 +173,7 @@ mysql_install() {
 	for package in libaio net-tools; do
 		if ! rpm -q $package >/dev/null 2>&1; then
 			yum install $package -y >/dev/null 2>&1
-			printf "${yellow} 安装并检查依赖${package}${white}\n"
+			printf "${yellow} 安装并检查依赖:${package}${white}\n"
 		fi
 	done
 
@@ -64,50 +191,114 @@ mysql_install() {
 		mysql-community-client-8.0.26-1.el7.x86_64.rpm \
 		mysql-community-server-8.0.26-1.el7.x86_64.rpm; do
 		if ! yum localinstall "$package" -y >/dev/null 2>&1; then
-			printf "${red}安装失败: ${package}${white}\n"
+			printf "${red}安装失败:${package}${white}\n"
 			exit 1
 		fi
-		printf "${green}安装成功: ${package}${white}\n"
+		printf "${green}安装成功:${package}${white}\n"
 	done
 
 	# 验证安装
-	printf "${yellow}验证 MySQL 安装${white}\n"
-	if mysql --version >/dev/null 2>&1 && mysqladmin --version >/dev/null 2>&1; then
-		printf "${green}MySQL安装成功,MySQL 版本: $(mysql --version)${white}\n"
-	else
-		printf "${red}MySQL安装失败,未找到相关版本信息.${white}\n"
-		exit 1
-	fi
+	check_mysql_installed
 
 	# 清理安装包文件
 	rm -f mysql-community*
 
-	# MySQL初始化
-	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '1xBSKI7#*6QRdv';" > /tmp/mysql-init
-	mysqld --initialize --user=mysql --init-file=/tmp/mysql-init
-	rm -f /tmp/mysql-init
-	printf "${green}通过指定配置文件初始化MySQL成功${white}\n"
+	# 备份当前的my.cnf文件
+	mv /etc/my.cnf{,.bak}
+	# 下载基础配置文件my-4C8G.cnf
+	generate_mysql_config
+
+	# 根据服务器性能调优
+	# 获取CPU核数和内存大小（单位为MB）
+	# local num_cores=$(grep -c ^processor /proc/cpuinfo)
+	local num_cores=$(nproc)
+	local memory_size=$(free -m | awk '/^Mem:/{print $2}')
+
+	# 根据内存大小调整参数
+	if [ "$memory_size" -ge 7500 ] && [ "$memory_size" -le 8000 ]; then
+		printf "${yellow}8GB内存配置,使用默认配置${white}\n"
+	elif [ "$memory_size" -ge 3500 ] && [ "$memory_size" -le 4000 ]; then
+		sed -i "s/^max_connections=.*/max_connections=300/" /etc/my.cnf
+		sed -i "s/^tmp_table_size=.*/tmp_table_size=16M/" /etc/my.cnf
+		sed -i "s/^myisam_sort_buffer_size=.*/myisam_sort_buffer_size=32M/" /etc/my.cnf
+		sed -i "s/^innodb_log_buffer_size=.*/innodb_log_buffer_size=128M/" /etc/my.cnf
+		sed -i "s/^innodb_buffer_pool_size=.*/innodb_buffer_pool_size=512M/" /etc/my.cnf
+		sed -i "s/^innodb_log_file_size=.*/innodb_log_file_size=512M/" /etc/my.cnf
+		sed -i "s/^innodb_open_files=.*/innodb_open_files=300/" /etc/my.cnf
+		sed -i "s/^max_allowed_packet=.*/max_allowed_packet=128M/" /etc/my.cnf
+		sed -i "s/^max_connect_errors=.*/max_connect_errors=50/" /etc/my.cnf
+		sed -i "s/^max_binlog_size=.*/max_binlog_size=256M/" /etc/my.cnf
+		sed -i "s/^read_rnd_buffer_size =.*/read_rnd_buffer_size = 512K/" /etc/my.cnf
+		sed -i "s/^read_buffer_size =.*/read_buffer_size = 512K/" /etc/my.cnf
+		sed -i "s/^sort_buffer_size =.*/sort_buffer_size = 512K/" /etc/my.cnf
+		printf "${yellow}MySQL配置文件已更新并根据服务器配置动态调整完成.${white}\n"
+	elif [ "$memory_size" -ge 1500 ] && [ "$memory_size" -le 2000 ]; then
+		sed -i "s/^max_connections=.*/max_connections=200/" /etc/my.cnf
+		sed -i "s/^tmp_table_size=.*/tmp_table_size=8M/" /etc/my.cnf
+		sed -i "s/^myisam_sort_buffer_size=.*/myisam_sort_buffer_size=16M/" /etc/my.cnf
+		sed -i "s/^innodb_log_buffer_size=.*/innodb_log_buffer_size=64M/" /etc/my.cnf
+		sed -i "s/^innodb_buffer_pool_size=.*/innodb_buffer_pool_size=256M/" /etc/my.cnf
+		sed -i "s/^innodb_log_file_size=.*/innodb_log_file_size=256M/" /etc/my.cnf
+		sed -i "s/^innodb_open_files=.*/innodb_open_files=200/" /etc/my.cnf
+		sed -i "s/^max_allowed_packet=.*/max_allowed_packet=64M/" /etc/my.cnf
+		sed -i "s/^max_connect_errors=.*/max_connect_errors=20/" /etc/my.cnf
+		sed -i "s/^max_binlog_size=.*/max_binlog_size=128M/" /etc/my.cnf
+		sed -i "s/^read_rnd_buffer_size =.*/read_rnd_buffer_size = 256K/" /etc/my.cnf
+		sed -i "s/^read_buffer_size =.*/read_buffer_size = 256K/" /etc/my.cnf
+		sed -i "s/^sort_buffer_size =.*/sort_buffer_size = 256K/" /etc/my.cnf
+		printf "${yellow}MySQL配置文件已更新并根据服务器配置动态调整完成.{white}\n"
+	else
+		printf "${red}未知内存配置,未做修改.${white}\n"
+	fi
+
+	# 调用函数进行MySQL初始化
+	if initialize_mysql; then
+		printf "${green}MySQL初始化成功!${white}\n"
+	else
+		printf "${red}MySQL初始化失败.${white}\n"
+		return
+	fi
+	
+	sudo systemctl enable mysqld --now >/dev/null 2>&1
+	
+	# 检查MySQL服务是否处于活动状态
+	if ! sudo systemctl is-active mysqld >/dev/null 2>&1; then
+		printf "${red}错误:MySQL状态检查失败或服务无法启动,请检查安装日志或手动启动MySQL服务. ${white}\n"
+		return 1
+	else
+		printf "${green}MySQL已完成自检,启动并设置开机自启. ${white}\n"
+	fi
+	
+	echo ""
 }
 
+# MySQL卸载
 mysql_uninstall() {
+	check_mysql_installed
+	if [[ $? -ne 0 ]]; then
+		printf "${red}MySQL未安装.${white}\n"
+		return
+	fi
+
 	local MAXDEPTH=5
 
-	sudo systemctl is-active mysqld >/dev/null 2>&1 && sudo systemctl stop mysqld >/dev/null 2>&1
+	sudo systemctl is-active mysqld >/dev/null 2>&1 && sudo systemctl disable mysqld --now >/dev/null 2>&1
 
 	# 遍历并卸载每个已安装的 MySQL 包
 	for package in $(rpm -qa | grep -iE '^mysql-community-'); do
 		if yum remove "$package" -y; then
-			printf "${green}成功卸载: $package${white}\n"
+			printf "${green}成功卸载:$package${white}\n"
 		else
-			printf "${red}卸载失败: $package${white}\n"
+			printf "${red}卸载失败:$package${white}\n"
 		fi
 	done
 
 	# 查找并删除与MySQL相关的文件夹
-	find / -maxdepth $MAXDEPTH -type d -name '*mysql*' 2>/dev/null | xargs rm -rf
+	[ -f /var/log/mysqld.log ] && rm -f /var/log/mysqld.log || true
+	find / -maxdepth $MAXDEPTH -type d -name '*mysql*' 2>/dev/null | xargs rm -fr
 
 	[ -f /etc/my.cnf ] && rm -f /etc/my.cnf
-
+	printf "${green}MySQL卸载完成.${white}\n"
 }
 
 ##########
@@ -127,7 +318,7 @@ main_menu() {
 				mysql_menu
 				;;
 			2)
-				printf "${yellow}退出菜单${white}\n"
+				printf "${yellow}Bye!${white}\n"
 				break  # 退出主菜单循环
 				;;
 			*)
