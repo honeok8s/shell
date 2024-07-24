65a3a1554953448c45ad7e30826bdf77f08ad5d0
honeok8s <honeok7@gmail.com>
2024-07-24 22:47:16 +0800

    优化安装逻辑

--- server_manager.sh
+++ server_manager.sh
@@ -2,15 +2,17 @@
 
 set -o errexit
 
-os_release=$(grep '^PRETTY_NAME=' /etc/os-release | cut -d '"' -f 2 | sed 's/ (.*//')
+# ANSI颜色码,用于彩色输出
+yellow='\033[1;33m' # 提示信息
+red='\033[1;31m'    # 警告信息
+green='\033[1;32m'  # 成功信息
+blue='\033[1;34m'   # 一般信息
+cyan='\033[1;36m'   # 特殊信息
+purple='\033[1;35m' # 紫色或粉色信息
+gray='\033[1;30m'   # 灰色信息
+white='\033[0m'     # 结束颜色设置
 
-# ANSI颜色码，用于彩色输出
-yellow='\033[1;33m'
-red='\033[1;31m'
-green='\033[1;32m'
-purple='\033[1;35m'
-cyan='\033[1;36m'
-white='\033[0m'
+os_release=$(grep '^PRETTY_NAME=' /etc/os-release | cut -d '"' -f 2 | sed 's/ (.*//')
 
 # 安装路径和备份路径
 SOFTWARE_DIR="/opt/software"
