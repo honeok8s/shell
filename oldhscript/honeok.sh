52d4cc0dcac174a23a88626436198a9ed79d2eb1
honeok8s <honeok7@gmail.com>
2024-07-31 13:43:23 +0800

    update honeok.sh

--- oldhscript/honeok.sh
+++ oldhscript/honeok.sh
@@ -5,15 +5,14 @@ set -o errexit
 clear
 
 # ANSI颜色码,用于彩色输出
-yellow='\033[1;33m'  # 提示信息
-red='\033[1;31m'     # 警告信息
-magenta='\033[0;35m' # 品红色
-green='\033[1;32m'   # 成功信息
-blue='\033[1;34m'    # 一般信息
-cyan='\033[1;36m'    # 特殊信息
-purple='\033[1;35m'  # 紫色或粉色信息
-gray='\033[1;30m'    # 灰色信息
-white='\033[0m'      # 结束颜色设置
+yellow='\033[1;33m' # 提示信息
+red='\033[1;31m'    # 警告信息
+green='\033[1;32m'  # 成功信息
+blue='\033[1;34m'   # 一般信息
+cyan='\033[1;36m'   # 特殊信息
+purple='\033[1;35m' # 紫色或粉色信息
+gray='\033[1;30m'   # 灰色信息
+white='\033[0m'     # 结束颜色设置
 
 system_info() {
 	local hostname=$(hostnamectl | sed -n 's/^[[:space:]]*Static hostname:[[:space:]]*\(.*\)$/\1/p')
@@ -47,20 +46,6 @@ system_info() {
 		}' <(sleep 1; cat /proc/stat))
 	local mem_usage=$(free -b | awk 'NR==2{printf "%.2f/%.2f MB (%.2f%%)", $3/1024/1024, $2/1024/1024, $3*100/$2}')
 	local swap_usage=$(free -m | awk 'NR==3{used=$3; total=$2; if (total == 0) {percentage=0} else {percentage=used*100/total}; printf "%dMB/%dMB (%d%%)", used, total, percentage}')
-
-	# 获取并格式化磁盘空间使用情况
-	local disk_info=$(df -h --output=source,size,used,pcent | grep -E "^/dev/" | grep -vE "tmpfs|devtmpfs|overlay|swap|loop")
-	local disk_output=""
-
-	while read -r line; do
-		local disk=$(echo "$line" | awk '{print $1}')
-		local size=$(echo "$line" | awk '{print $2}')
-		local used=$(echo "$line" | awk '{print $3}')
-		local percent=$(echo "$line" | awk '{print $4}')
-
-		# 拼接磁盘信息
-		disk_output+="${disk} ${used}/${size} (${percent})  "
-	done <<< "$disk_info"
 
 	# 将字节数转换为GB
 	bytes_to_gb() {
@@ -110,38 +95,52 @@ system_info() {
 
 	local uptime_str=$(uptime | awk -F'up ' '{print $2}' | awk -F', ' '{days=$1; hours_minutes=$2; gsub(/[^0-9 ]/, "", days); split(hours_minutes, a, ":"); hours=a[1]; minutes=a[2]; printf "%d天 %d时 %d分\n", days, hours, minutes}')
 
-	echo ""
-	echo -e "${yellow}系统信息查询${white}"
-	echo "-------------------------"
-	echo "主机名: ${hostname}"
-	echo "运营商: ${isp_info}"
-	echo "-------------------------"
-	echo "操作系统: ${os_release}"
-	echo "内核版本: ${kernel_version}"
-	echo "-------------------------"
-	echo "CPU架构: ${cpu_architecture}"
-	echo "CPU型号: ${cpu_model}"
-	echo "CPU核心: ${cpu_cores}"
-	echo "-------------------------"
-	echo "CPU占用率: ${cpu_usage}"
-	echo "物理内存: ${mem_usage}"
-	echo "虚拟内存: ${swap_usage}"
-	echo "硬盘空间: ${disk_output}"
-	echo "-------------------------"
-	echo "网络接收数据量: $(bytes_to_gb $total_recv_bytes)"
-	echo "网络发送数据量: $(bytes_to_gb $total_sent_bytes)"
-	echo "-------------------------"
-	echo "网络拥塞控制算法: ${congestion_algorithm} ${queue_algorithm}"
-	echo "-------------------------"
-	echo "公网IPv4地址: ${ipv4_address}"
-	echo "公网IPv6地址: ${ipv6_address}"
-	echo "-------------------------"
-	echo "地理位置: ${location}"
-	echo "系统时区: ${system_time}"
-	echo "系统时间: ${current_time}"
-	echo "运行时长: ${uptime_str}"
-	echo "-------------------------"
-	echo
+	printf "${purple}系统信息查询${white}\n"
+	printf "${yellow}-------------------------${white}\n"
+	printf "${yellow}主机名: ${hostname}${white}\n"
+	printf "${yellow}运营商: ${isp_info}${white}\n"
+	printf "${yellow}-------------------------${white}\n"
+	printf "${yellow}操作系统: ${os_release}${white}\n"
+	printf "${yellow}内核版本: ${kernel_version}${white}\n"
+	printf "${yellow}-------------------------${white}\n"
+	printf "${yellow}CPU架构: ${cpu_architecture}${white}\n"
+	printf "${yellow}CPU型号: ${cpu_model}${white}\n"
+	printf "${yellow}CPU核心: ${cpu_cores}${white}\n"
+	printf "${yellow}-------------------------${white}\n"
+	printf "${yellow}CPU占用率: %s${white}\n" "${cpu_usage}"
+	printf "${yellow}物理内存: %s${white}\n" "${mem_usage}"
+	printf "${yellow}虚拟内存: %s${white}\n" "${swap_usage}"
+
+	# 获取并格式化磁盘空间使用情况
+	local disk_info=$(df -h --output=source,size,used,pcent | grep -E "^/dev/" | grep -vE "tmpfs|devtmpfs|overlay|swap|loop")
+	local disk_output=""
+
+	while read -r line; do
+		local disk=$(echo "$line" | awk '{print $1}')
+		local size=$(echo "$line" | awk '{print $2}')
+		local used=$(echo "$line" | awk '{print $3}')
+		local percent=$(echo "$line" | awk '{print $4}')
+		
+		# 拼接磁盘信息
+		disk_output+="${disk} ${used}/${size} (${percent})  "
+	done <<< "$disk_info"
+	# 打印硬盘空间
+	printf "${yellow}硬盘空间: %s${white}\n" "${disk_output}"
+
+	printf "${yellow}-------------------------${white}\n"
+	printf "${yellow}网络接收数据量: $(bytes_to_gb $total_recv_bytes)${white}\n"
+	printf "${yellow}网络发送数据量: $(bytes_to_gb $total_sent_bytes)${white}\n"
+	printf "${yellow}-------------------------${white}\n"
+	printf "${yellow}网络拥塞控制算法: ${congestion_algorithm} ${queue_algorithm}${white}\n"
+	printf "${yellow}-------------------------${white}\n"
+	printf "${yellow}公网IPv4地址: ${ipv4_address}${white}\n"
+	printf "${yellow}公网IPv6地址: ${ipv6_address}${white}\n"
+	printf "${yellow}-------------------------${white}\n"
+	printf "${yellow}地理位置: ${location}${white}\n"
+	printf "${yellow}系统时区: ${system_time}${white}\n"
+	printf "${yellow}系统时间: ${current_time}${white}\n"
+	printf "${yellow}运行时长: ${uptime_str}${white}\n"
+	printf "${yellow}-------------------------${white}\n"
 }
 
 system_info
