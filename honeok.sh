0a2f8219b046315b180bc5128b5a330a2c72461c
honeok8s <honeok7@gmail.com>
2024-08-04 03:26:44 +0800

    update

--- honeok.sh
+++ honeok.sh
@@ -7,13 +7,13 @@ clear
 
 yellow='\033[1;33m'  # 提示信息
 red='\033[1;31m'     # 警告信息
-magenta='\033[0;35m' # 品红色
+magenta='\033[1;35m' # 品红色
 green='\033[1;32m'   # 成功信息
 blue='\033[1;34m'    # 一般信息
 cyan='\033[1;36m'    # 特殊信息
 purple='\033[1;35m'  # 紫色或粉色信息
 gray='\033[1;30m'    # 灰色信息
-orange='\033[38;5;208m'  # 橘色
+orange='\033[1;38;5;208m'
 white='\033[0m'      # 结束颜色设置
 _yellow() { echo -e ${yellow}$@${white}; }
 _red() { echo -e ${red}$@${white}; }
