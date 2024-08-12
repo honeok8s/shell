436bb1aead8f4c9145a4ccbe5c7c6a0f15d7e11f
honeok8s <honeok7@gmail.com>
2024-08-12 15:10:26 +0800

    update

--- docker/install.sh
+++ docker/install.sh
@@ -5,19 +5,19 @@
 set -o errexit
 clear
 
-gitdocker_version="v0.1 2024.8.7"
+gitdocker_version="2024.8.12 v1.0"
 os_release=$(grep '^PRETTY_NAME=' /etc/os-release | cut -d '"' -f 2)
 
-yellow='\033[1;33m'  # 提示信息
-red='\033[1;31m'     # 警告信息
-magenta='\033[1;35m' # 品红色
-green='\033[1;32m'   # 成功信息
-blue='\033[1;34m'    # 一般信息
-cyan='\033[1;36m'    # 特殊信息
-purple='\033[1;35m'  # 紫色或粉色信息
-gray='\033[1;30m'    # 灰色信息
-orange='\033[1;38;5;208m'
-white='\033[0m'      # 结束颜色设置
+yellow='\033[1;33m'       # 黄色
+red='\033[1;31m'          # 红色
+magenta='\033[1;35m'      # 品红色
+green='\033[1;32m'        # 绿色
+blue='\033[1;34m'         # 蓝色
+cyan='\033[1;36m'         # 青色
+purple='\033[1;35m'       # 紫色
+gray='\033[1;30m'         # 灰色
+orange='\033[1;38;5;208m' # 橙色
+white='\033[0m'           # 白色
 _yellow() { echo -e ${yellow}$@${white}; }
 _red() { echo -e ${red}$@${white}; }
 _magenta() { echo -e ${magenta}$@${white}; }
