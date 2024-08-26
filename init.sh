aed9b46201a3104f8f0f8eb24f36a633b3f86bf7
honeok8s <honeok7@gmail.com>
2024-08-26 15:03:17 +0800

    update

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 # Author: honeok
 # Blog: https://www.honeok.com
 
-set -o errexit
+#set -o errexit
 
 yellow='\033[1;33m'       # 黄色
 red='\033[1;31m'          # 红色
@@ -24,6 +24,180 @@ _purple() { echo -e ${purple}$@${white}; }
 _gray() { echo -e ${gray}$@${white}; }
 _orange() { echo -e ${orange}$@${white}; }
 
+#################### 通用函数START ####################
+# 安装软件包
+install() {
+	if [ $# -eq 0 ]; then
+		_red "未提供软件包参数"
+		return 1
+	fi
+
+	for package in "$@"; do
+		if ! command -v "$package" &>/dev/null; then
+			_yellow "正在安装$package"
+			if command -v dnf &>/dev/null; then
+				dnf update -y
+				dnf install epel-release -y
+				dnf install "$package" -y
+			elif command -v yum &>/dev/null; then
+				yum update -y
+				yum install epel-release -y
+				yum install "$package" -y
+			elif command -v apt &>/dev/null; then
+				apt update -y
+				apt install "$package" -y
+			elif command -v apk &>/dev/null; then
+				apk update
+				apk add "$package"
+			else
+				_red "未知的包管理器"
+				return 1
+			fi
+		else
+			_green "$package已安装"
+		fi
+	done
+
+	return 0
+}
+
+# 卸载软件包
+remove() {
+	if [ $# -eq 0 ]; then
+		_red "未提供软件包参数"
+		return 1
+	fi
+
+	for package in "$@"; do
+		_yellow "正在卸载$package"
+		if command -v dnf &>/dev/null; then
+			if rpm -q "$package" &>/dev/null; then
+				dnf remove "$package"* -y
+			fi
+		elif command -v yum &>/dev/null; then
+			if rpm -q "${package}" >/dev/null 2>&1; then
+				yum remove "${package}"* -y
+			fi
+		elif command -v apt &>/dev/null; then
+			if dpkg -l | grep -qw "${package}"; then
+				apt purge "${package}"* -y
+			fi
+		elif command -v apk &>/dev/null; then
+			if apk info | grep -qw "${package}"; then
+				apk del "${package}"*
+			fi
+		else
+			_red "未知的包管理器"
+			return 1
+		fi
+	done
+
+	return 0
+}
+
+# 通用systemctl函数,适用于各种发行版
+systemctl() {
+	local cmd="$1"
+	local service_name="$2"
+
+	if command -v apk &>/dev/null; then
+		service "$service_name" "$cmd"
+	else
+		/bin/systemctl "$cmd" "$service_name"
+	fi
+}
+
+# 重载systemd管理的服务
+daemon_reload() {
+	if command -v apk &>/dev/null; then
+		# Alpine使用OpenRC
+		rc-service -a
+	else
+		/bin/systemctl daemon-reload
+	fi
+}
+
+disable() {
+	local service_name="$1"
+	if command -v apk &>/dev/null; then
+		# Alpine使用OpenRC
+		rc-update del "$service_name"
+	else
+		/bin/systemctl disable "$service_name"
+	fi
+}
+
+# 设置服务为开机自启
+enable() {
+	local service_name="$1"
+	if command -v apk &>/dev/null; then
+		rc-update add "$service_name" default
+	else
+		systemctl enable "$service_name"
+	fi
+
+	if [ $? -eq 0 ]; then
+		_green "$service_name已设置为开机自启"
+	else
+		_red "$service_name设置开机自启失败"
+	fi
+}
+
+# 启动服务
+start() {
+	local service_name="$1"
+	systemctl start "$service_name"
+	if [ $? -eq 0 ]; then
+		_green "$service_name已启动"
+	else
+		_red "$service_name启动失败"
+	fi
+}
+
+# 停止服务
+stop() {
+	local service_name="$1"
+	systemctl stop "$service_name"
+	if [ $? -eq 0 ]; then
+		_green "$service_name已停止"
+	else
+		_red "$service_name停止失败"
+	fi
+}
+
+# 重启服务
+restart() {
+	local service_name="$1"
+	systemctl restart "$service_name"
+	if [ $? -eq 0 ]; then
+		_green "$service_name已重启"
+	else
+		_red "$service_name重启失败"
+	fi
+}
+
+# 重载服务
+reload() {
+	local service_name="$1"
+	systemctl reload "$service_name"
+	if [ $? -eq 0 ]; then
+		_green "$service_name已重载"
+	else
+		_red "$service_name重载失败"
+	fi
+}
+
+# 查看服务状态
+status() {
+	local service_name="$1"
+	systemctl status "$service_name"
+	if [ $? -eq 0 ]; then
+		_green "$service_name状态已显示"
+	else
+		_red "$service_name状态显示失败"
+	fi
+}
+
 # 结尾任意键结束
 end_of(){
 	_green "操作完成"
@@ -39,133 +213,32 @@ need_root(){
 	if [ "$(id -u)" -ne "0" ]; then
 		_red "该功能需要root用户才能运行"
 		end_of
+		# 回调主菜单
+		honeok
 	fi
 }
 
-# 安装软件包
-install(){
-	if [ $# -eq 0 ]; then
-		_red "未提供软件包参数"
-		return 1
-	fi
+# 获取公网IP地址
+ip_address() {
+	local ipv4_services=("ipv4.ip.sb" "api.ipify.org" "checkip.amazonaws.com" "ipinfo.io/ip")
+	local ipv6_services=("ipv6.ip.sb" "api6.ipify.org" "v6.ident.me" "ipv6.icanhazip.com")
 
-	for package in "$@"; do
-		if ! command -v "$package" &>/dev/null; then
-			_yellow "正在安装$package"
-			if command -v dnf &>/dev/null; then
-				dnf install -y "$package"
-			elif command -v yum &>/dev/null; then
-				yum -y install "$package"
-			elif command -v apt &>/dev/null; then
-				apt update && apt install -y "$package"
-			elif command -v apk &>/dev/null; then
-				apk add "$package"
-			else
-				_red "未知的包管理器"
-				return 1
-			fi
-		else
-			_green "$package已经安装"
+	# 获取IPv4地址
+	for service in "${ipv4_services[@]}"; do
+		ipv4_address=$(curl -s "$service")
+		if [[ $ipv4_address =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+			break
 		fi
 	done
-	return 0
-}
 
-# 卸载软件包
-remove(){
-	if [ $# -eq 0 ]; then
-		_red "未提供软件包参数"
-		return 1
-	fi
-
-	for package in "$@"; do
-		_yellow "正在卸载$package"
-		if command -v dnf &>/dev/null; then
-			dnf remove -y "${package}"*
-		elif command -v yum &>/dev/null; then
-			yum remove -y "${package}"*
-		elif command -v apt &>/dev/null; then
-			apt purge -y "${package}"*
-		elif command -v apk &>/dev/null; then
-			apk del "${package}"*
+	# 获取IPv6地址
+	for service in "${ipv6_services[@]}"; do
+		ipv6_address=$(curl -s --max-time 1 "$service")
+		if [[ $ipv6_address =~ ^[0-9a-fA-F:]+$ ]]; then
+			break
 		else
-			_red "未知的包管理器"
-			return 1
+			ipv6_address=""
 		fi
 	done
-	return 0
 }
-
-# 通用systemctl函数, 适用于各种发行版
-systemctl() {
-	local COMMAND="$1"
-	local SERVICE_NAME="$2"
-
-	if command -v apk &>/dev/null; then
-		service "$SERVICE_NAME" "$COMMAND"
-	else
-		/bin/systemctl "$COMMAND" "$SERVICE_NAME"
-	fi
-}
-
-# 重启服务
-restart() {
-	systemctl restart "$1"
-	if [ $? -eq 0 ]; then
-		_green "$1服务已重启"
-	else
-		_red "错误:重启$1服务失败"
-	fi
-}
-
-# 重载服务
-reload() {
-	systemctl reload "$1"
-	if [ $? -eq 0 ]; then
-		_green "$1服务已重载"
-	else
-		_red "错误:重载$1服务失败"
-	fi
-}
-
-# 启动服务
-start() {
-	systemctl start "$1"
-	if [ $? -eq 0 ]; then
-		_green "$1服务已启动"
-	else
-		_red "错误:启动$1服务失败"
-	fi
-}
-
-# 停止服务
-stop() {
-	systemctl stop "$1"
-	if [ $? -eq 0 ]; then
-		_green "$1服务已停止"
-	else
-		_red "错误:停止$1服务失败"
-	fi
-}
-
-# 查看服务状态
-status() {
-	systemctl status "$1"
-	if [ $? -eq 0 ]; then
-		_green "$1服务状态已显示"
-	else
-		_red "错误:无法显示$1服务状态"
-	fi
-}
-
-# 设置服务为开机自启
-enable() {
-	local service_name="$1"
-	if command -v apk &>/dev/null; then
-		rc-update add "$service_name" default
-	else
-		/bin/systemctl enable "$service_name"
-	fi
-
-	_green "$service_name已设置为开机自启"
-}
+#################### 通用函数END ####################
