5690168b52d971f61647496b0dab34d92e97a7e0
honeok8s <honeok7@gmail.com>
2024-10-13 21:12:33 +0800

    update

--- honeok_dev.sh
+++ honeok_dev.sh
@@ -36,7 +36,7 @@ set_region_config() {
 		execute_commands=0                    # 0 表示允许执行命令
 		
 		# 定义局部变量
-		local github_proxies=("gh-proxy.com" "gh.kejilion.pro" "github.moeyy.xyz" "www.ghproxy.cn" "ghp.ci")
+		local github_proxies=("gh-proxy.com" "gh.kejilion.pro" "github.moeyy.xyz")
 		local best_proxy=""
 		local best_time=9999                  # 设置一个较大的初始延迟值
 		local ping_time=""
